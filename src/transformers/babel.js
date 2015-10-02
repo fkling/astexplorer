6e02f8ee680c3507e390e0cad2917f4b581582f8
Felix Kling <felix.kling@gmx.net>
2015-10-02 07:54:49 -0700

    Enable stage 0 for Babel transformer

--- src/transformers/babel.js
+++ src/transformers/babel.js
@@ -8,19 +8,28 @@ const ID = 'babel';
 const defaultTransform =
   fs.readFileSync(__dirname + '/transformBabel.txt', 'utf8');
 
+const options = {
+  stage: 0,
+};
+
 function transform({transformCode, code}) {
   return new Promise((resolve, reject) => {
     loadjs(['babel-core'], babel => {
       try {
         // This might throw
-        var transform = compileModule(
-          babel.transform(transformCode).code
+        let transform = compileModule( // eslint-disable-line no-shadow
+          babel.transform(transformCode, options).code
         );
 
-        resolve(babel.transform(
-          code,
-          {plugins: [transform]},
-        ).code);
+        resolve(
+            babel.transform(
+              code,
+              {
+                ...options,
+                plugins: [transform],
+              }
+            ).code
+        );
       } catch(ex) {
         reject(ex);
       }
