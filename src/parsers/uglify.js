5871584c86838e3b111903ec9d2f252d9fc2a030
Ingvar Stepanyan <me@rreverser.com>
2015-11-23 00:18:42 +0000

    Inherit Uglify parser from non-ESTree defaultParserInterface.

--- src/parsers/uglify.js
+++ src/parsers/uglify.js
@@ -1,5 +1,4 @@
-import React from 'react'; // eslint-disable-line no-unused-vars
-import defaultParserInterface from './utils/defaultESTreeParserInterface';
+import defaultParserInterface from './utils/defaultParserInterface';
 import pkg from 'uglify-js/package.json';
 import SettingsRenderer from './utils/SettingsRenderer';
 import * as LocalStorage from '../LocalStorage';
@@ -63,6 +62,15 @@ export default {
         break;
     }
     return [start.pos, end.endpos];
+  },
+
+  opensByDefault(node, key) {
+    return (
+      key === 'body' ||
+      key === 'elements' || // array literals
+      key === 'definitions' || // variable declaration
+      key === 'properties'
+    );
   },
 
   _ignoredProperties: new Set(['_walk', 'CTOR']),
