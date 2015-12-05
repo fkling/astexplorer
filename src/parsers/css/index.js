ae51fe2be2b39545fc31bb06b04d4ae728df9fa3
Ingvar Stepanyan <me@rreverser.com>
2015-12-05 17:26:56 +0000

    Use WebPack context to simplify adding parsers and categories.

--- src/parsers/css/index.js
+++ src/parsers/css/index.js
@@ -1,12 +1,2 @@
-export { default as codeExample } from './codeExample.txt';
-
-import rework from './rework';
-import postcss from './postcss';
-
 export const id = 'css';
 export const displayName = 'CSS';
-
-export const parsers = [
-  rework,
-  postcss,
-];
--- src/parsers/index.js
+++ src/parsers/index.js
@@ -1,29 +1,42 @@
-import * as js from './js';
-import * as css from './css';
+const requireParser = require.context('./', true, /^\.\/(?!utils)[^/]+\/(codeExample\.txt|[^/]+?\.js)$/);
 
-export const categories = [
-  js,
-  css,
-];
+const files =
+  requireParser.keys()
+  .map(name => name.match(/\.\/(.*?)\/(.*)/).slice(1));
+
+const categoryByID = {};
+const parserByID = {};
+
+export const categories =
+  files
+  .filter(([, fileName]) => fileName === 'index.js')
+  .map(([catName]) => {
+    let category = requireParser(`./${catName}/index.js`);
+    categoryByID[category.id] = category;
+    category.codeExample = requireParser(`./${catName}/codeExample.txt`);
+    category.parsers =
+      files
+      .filter(([curCatName, curFileName]) => (
+        curCatName === catName &&
+        curFileName !== 'index.js' &&
+        curFileName !== 'codeExample.txt'
+      ))
+      .map(([catName, curParserName]) => {
+        let parser = requireParser(`./${catName}/${curParserName}`);
+        parserByID[parser.id] = parser;
+        parser.category = category;
+        return parser;
+      });
+    return category;
+  });
 
 export function getDefaultCategory() {
-  return categories[0];
+  return categoryByID.javascript;
 }
 
 export function getDefaultParser(category = getDefaultCategory()) {
   return category.parsers[0];
 }
-
-let categoryByID = {};
-let parserByID = {};
-
-categories.forEach(category => {
-  categoryByID[category.id] = category;
-  category.parsers.forEach(parser => {
-    parser.category = category;
-    parserByID[parser.id] = parser;
-  });
-});
 
 export function getCategoryByID(id) {
   return categoryByID[id];
