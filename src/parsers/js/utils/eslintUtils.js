e63b24fbf19d1ee92f8284b93bc1ebe2ef3831b2
Daniel Lo Nigro <daniel@dan.cx>
2016-03-30 21:37:51 -0700

    Add support for ESLint v2

---
+++ src/parsers/js/utils/eslintUtils.js
@@ -0,0 +1,37 @@
+import compileModule from '../../utils/compileModule';
+import {parseNoPatch} from 'babel-eslint';
+import {transform} from 'babel-core';
+
+export function formatResults(results) {
+  return results.length === 0
+    ? 'Lint rule not fired.'
+    : results.map(formatResult).join('').trim();
+}
+
+export function formatResult(result) {
+  var pointer = '-'.repeat(result.column - 1) + '^';
+  return `
+// ${result.message} (at ${result.line}:${result.column})
+   ${result.source}
+// ${pointer}
+`;
+}
+
+export function defineRule(eslintRules, code) {
+  // Compile the transform code and install it as an ESLint rule. The rule
+  // name doesn't really matter here, so we'll just use a hard-coded name.
+  const rule = compileModule(transform(code).code);
+  eslintRules.define('astExplorerRule', rule);
+}
+
+export function runRule(code, eslint, sourceCode) {
+  // Run the ESLint rule on the AST of the provided code.
+  // Reference: http://eslint.org/docs/developer-guide/nodejs-api
+  const ast = parseNoPatch(code);
+  const results = eslint.verify(new sourceCode(code, ast), {
+    rules: {
+      astExplorerRule: 2
+    }
+  });
+  return formatResults(results);
+}
