9f01b3044e38acd61d3117b2e1305fb77f335194
Henry Zhu <hi@henryzoo.com>
2016-11-02 11:15:42 -0400

    update babel, babylon (#154)

--- webpack.config.js
+++ webpack.config.js
@@ -95,7 +95,7 @@ module.exports = Object.assign({
           babelrc: false,
           presets: [
             require.resolve('babel-preset-es2015'),
-            require.resolve('babel-preset-stage-1'),
+            require.resolve('babel-preset-stage-0'),
             require.resolve('babel-preset-react'),
           ],
           plugins: [
