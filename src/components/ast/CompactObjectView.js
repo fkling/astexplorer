9eda4777db5896bc6d1389ff94be94025193bdbc
skratchdot <git@skratchdot.com>
2016-12-09 00:16:37 -0500

    fixing eslint errors

--- src/components/ast/CompactObjectView.js
+++ src/components/ast/CompactObjectView.js
@@ -2,18 +2,18 @@ import React from 'react';
 
 export default function CompactObjectView({keys, onClick}) {
   if (keys.length === 0) {
-    return <span className="p">{"{ }"}</span>;
+    return <span className="p">{'{ }'}</span>;
   } else {
     if (keys.length > 5) {
       keys = keys.slice(0, 5).concat([`... +${keys.length - 5}`]);
     }
     return (
       <span>
-        <span className="p">{" {"}</span>
+        <span className="p">{' {'}</span>
         <span className="compact placeholder ge" onClick={onClick}>
           {keys.join(', ')}
         </span>
-        <span className="p">{"}"}</span>
+        <span className="p">{'}'}</span>
       </span>
     );
   }
