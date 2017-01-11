033626fb887fdef84962041364db2d6108e86927
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-01-11 21:09:16 +0100

    Add transform support to "glimmer"

---
+++ src/parsers/handlebars/transformers/glimmer/index.js
@@ -0,0 +1,34 @@
+import compileModule from '../../../utils/compileModule';
+import pkg from 'glimmer-syntax/package.json';
+
+const ID = 'glimmer';
+
+export default {
+  id: ID,
+  displayName: ID,
+  version: pkg.version,
+  homepage: pkg.homepage,
+
+  defaultParserID: 'glimmer',
+
+  loadTransformer(callback) {
+    require(['glimmer-syntax'], callback);
+  },
+
+  transform(glimmer, transformCode, code) {
+    const transformModule = compileModule(transformCode);
+
+    // allow "export default" instead of "module.exports = "
+    const transform = transformModule.__esModule ?
+      transformModule.default :
+      transformModule;
+
+    let ast = glimmer.preprocess(code, {
+      plugins: {
+        ast: [transform],
+      },
+    });
+
+    return glimmer.print(ast);
+  },
+};
