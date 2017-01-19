396c98e5fe82e77ab2afa025c2d293750d258139
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-01-19 11:07:39 +0100

    handlebars: Add "glimmer-compiler" transform
    This is essentially the same kind of transform as "glimmer" but with a different kind of output view

--- website/webpack.config.js
+++ website/webpack.config.js
@@ -90,7 +90,10 @@ module.exports = Object.assign({
         include: [
           path.join(__dirname, 'src'),
           path.join(__dirname, 'node_modules', 'jscodeshift', 'dist'),
+          path.join(__dirname, 'node_modules', '@glimmer', 'compiler', 'dist'),
           path.join(__dirname, 'node_modules', '@glimmer', 'syntax', 'dist'),
+          path.join(__dirname, 'node_modules', '@glimmer', 'util', 'dist'),
+          path.join(__dirname, 'node_modules', '@glimmer', 'wire-format', 'dist'),
         ],
         loader: 'babel',
         query: {
