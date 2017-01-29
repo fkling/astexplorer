1891167c1f5239ec6c20c5d6a45c96a0155c4544
Felix Kling <felix.kling@gmx.net>
2017-01-29 01:00:11 -0800

    Set cache control header for reading gists

--- server/handlers/loadGist.js
+++ server/handlers/loadGist.js
@@ -4,10 +4,14 @@ const GitHub = require('github-api');
 module.exports = function loadGist(req, res, next) {
   const gh = new GitHub({token: AUTH_TOKEN});
   const gist = gh.getGist(req.params.snippetid);
-  (req.params.revisionid === 'latest' ?
+  const latest = req.params.revisionid === 'latest';
+  (latest ?
     gist.read() :
     gist.getRevision(req.params.revisionid)
   )
-    .then(response => res.json(response.data))
+    .then(response => {
+      res.append('Cache-Control', latest ? 'no-cache' : 'max-age=1800, public');
+      res.json(response.data);
+    })
     .catch(next);
 };
