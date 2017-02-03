5d6ac3cb47d24dd01218c06de953ab0065028391
Felix Kling <felix.kling@gmx.net>
2017-02-03 01:05:15 -0800

    Disable babel runtime transform polyfill
    The polyfill feature of the runtime transform causes Babel to insert
    `import` statements, which leads to issues with webpack. Disabling the
    polyfill fixes that. See https://github.com/babel/babel/issues/2877 for
    more info.
    
    I don't think the polyfill is actually needed, most visitors seem to use
    modern browsers.

--- website/webpack.config.js
+++ website/webpack.config.js
@@ -139,7 +139,16 @@ module.exports = Object.assign({
             require.resolve('babel-preset-react'),
           ],
           plugins: [
-            require.resolve('babel-plugin-transform-runtime'),
+            [
+              require.resolve('babel-plugin-transform-runtime'),
+              // https://github.com/babel/babel/issues/2877 describes an issue
+              // where babel inserts untranspiled import statements into a
+              // module. That module then contains ES6 and CommonJS module code,
+              // leading to issues with webpack. Disabling the polyfill seems
+              // to fix it, and we probably don't need it anyway.
+              // (should also result in smaller bundles)
+              {polyfill: false},
+            ],
           ],
         },
       },
