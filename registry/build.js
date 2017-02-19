2a69e213ef1617ffbf8f0b36a50541ce4cba3b22
Felix Kling <felix.kling@gmx.net>
2017-02-19 15:42:05 -0800

    Use our own module loader
    ... with the hope that it allows us to do better error reporting.

--- registry/build.js
+++ registry/build.js
@@ -112,6 +112,7 @@ function bundleVersion(packagePath, versionConfig, npmPkg, latest) {
             filename: `${name}.js`,
             path: BUNDLE_DIR,
             libraryTarget: 'amd',
+            library: `${toolID}/${version}`,
           },
         }
       ),
--- website/src/registry.js
+++ website/src/registry.js
@@ -1,16 +1,5 @@
-import curl from 'exports-loader?window.curl!curl-amd/dist/curl/curl';
 import React from 'react';
 import api from './storage/api';
-
-curl.config({
-  baseUrl: '/api/v1/tools',
-  dontAddFileExt: /./,
-  defineName: 'astexplorerDefine',
-});
-
-global.astexplorerDefine('react', () => React);
-global.astexplorerDefine('regeneratorRuntime', () => global.regeneratorRuntime);
-
 
 export function loadParsers() {
   return api('/tools/inventory.json')
@@ -36,19 +25,12 @@ export function loadParsers() {
 
 export function loadParser(id) {
   const [,tool,version='latest'] = id.match(/^([^@]+)(?:@(.+))?$/);
-  return new Promise((resolve, reject) => {
-    curl(
-      [`${tool}/${version}`],
-      parser => {
-        parser = parser.default || parser;
-        parser.load = memorize(parser.load, parser);
-        resolve(parser);
-      },
-      error => {
-        reject(error);
-      }
-    );
-  });
+  return load(`${tool}/${version}`)
+    .then(parser => {
+      parser = parser.default || parser;
+      parser.load = memorize(parser.load, parser);
+      return parser;
+    });
 }
 
 function memorize(f, obj) {
@@ -60,3 +42,55 @@ function memorize(f, obj) {
     return memo = f.apply(obj, args);
   }
 }
+
+// Loader functions
+const modules = new Map();
+const pending = new Map();
+
+function load(id) {
+  if (modules.has(id)) {
+    return modules.get(id);
+  }
+  const promise = new Promise((resolve, reject) => {
+    pending.set(id, {resolve, reject});
+
+    const script = global.document.createElement('script');
+    script.async = true;
+    script.onerror = function(error) {
+      // TODO: error reporting
+      console.log(error);
+      reject(error);
+      pending.delete(id);
+    };
+    script.src = '/api/v1/tools/' + id;
+    global.document.head.appendChild(script);
+  });
+  modules.set(id, promise);
+  return promise;
+}
+
+
+
+function define(id, dependencies, factory) {
+  if (typeof dependencies === 'function') {
+    factory = dependencies;
+    dependencies = [];
+  }
+
+  Promise.all(dependencies.map(d => load(d))).then(deps => {
+    if (pending.has(id)) {
+      const resolver = pending.get(id);
+      try {
+        resolver.resolve(factory(...deps));
+      } catch(e) {
+        resolver.reject(e);
+      } finally {
+        pending.delete(id);
+      }
+    }
+  });
+}
+global.astexplorerDefine = define;
+
+// Prime modules
+modules.set('react', Promise.resolve(React));
