df29de80360f7d6426935a87772434f720207fe3
Titus Wormer <tituswormer@gmail.com>
2017-05-17 13:16:24 +0200

    Add markdown (remark)

---
+++ website/src/parsers/markdown/remark.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import defaultParserInterface from '../utils/defaultParserInterface';
+import pkg from 'remark/package.json';
+import SettingsRenderer from '../utils/SettingsRenderer';
+
+const ID = 'remark';
+const defaultOptions = {
+  gfm: true,
+  yaml: true,
+  commonmark: false,
+  footnotes: false,
+};
+
+const parserSettingsConfiguration = {
+  fields: Object.keys(defaultOptions),
+};
+
+export default {
+  ...defaultParserInterface,
+
+  id: ID,
+  displayName: ID,
+  version: pkg.version,
+  homepage: pkg.homepage,
+  locationProps: new Set(['position']),
+
+  loadParser(callback) {
+    require(['remark'], callback);
+  },
+
+  parse(remark, code, options) {
+    return remark()
+      .data('settings', {...defaultOptions, ...options})
+      .parse(code);
+  },
+
+  nodeToRange({position}) {
+    if (position) {
+      return [position.start.offset, position.end.offset];
+    }
+  },
+
+  getNodeName(node) {
+    return node.type;
+  },
+
+  opensByDefault(node, key) {
+    return key === 'children';
+  },
+
+  renderSettings(parserSettings, onChange) {
+    return (
+      <div>
+        <p>
+          <a
+            href="https://github.com/wooorm/remark/tree/master/packages/remark-parse#options"
+            target="_blank">
+            Option descriptions
+          </a>
+        </p>
+        <SettingsRenderer
+          settingsConfiguration={parserSettingsConfiguration}
+          parserSettings={{...defaultOptions, ...parserSettings}}
+          onChange={onChange}
+        />
+      </div>
+    );
+  },
+};
