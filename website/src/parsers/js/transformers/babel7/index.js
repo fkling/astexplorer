182fb696c75adc219322abc4a688b9fb6ed305c6
Henry Zhu <hi@henryzoo.com>
2017-05-25 11:52:36 -0400

    babel 7

---
+++ website/src/parsers/js/transformers/babel7/index.js
@@ -0,0 +1,51 @@
+import compileModule from '../../../utils/compileModule';
+import pkg from 'babel7/babel7-package';
+
+const ID = 'babelv7';
+
+export default {
+  id: ID,
+  displayName: ID,
+  version: pkg.version,
+  homepage: pkg.homepage,
+
+  defaultParserID: 'babylon7',
+
+  loadTransformer(callback) {
+    require([
+      'babel7',
+      'recast',
+    ], (babel, recast) => callback({ babel, recast }));
+  },
+
+  transform({ babel, recast }, transformCode, code) {
+    let transform = compileModule( // eslint-disable-line no-shadow
+      transformCode
+    );
+
+    return babel.transform(code, {
+      parserOpts: {
+        parser: recast.parse,
+        plugins: [
+          'asyncGenerators',
+          'classPrivateProperties',
+          'classProperties',
+          'decorators',
+          'doExpressions',
+          'exportExtensions',
+          'flow',
+          'functionSent',
+          'functionBind',
+          'jsx',
+          'objectRestSpread',
+          'dynamicImport',
+        ],
+      },
+      generatorOpts: {
+        generator: recast.print,
+      },
+      plugins: [(transform.default || transform)(babel)],
+      sourceMaps: true,
+    });
+  },
+};
