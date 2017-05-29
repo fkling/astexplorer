e8071b4c1366a51ca246cc2c8221d3abc72d2e54
Henry Zhu <hi@henryzoo.com>
2017-05-29 17:11:34 -0400

    babel 7 (#231)

---
+++ website/src/parsers/js/babylon7.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import defaultParserInterface from './utils/defaultESTreeParserInterface';
+import pkg from 'babylon7/babylon-package';
+import SettingsRenderer from '../utils/SettingsRenderer';
+
+const availablePlugins = [
+  'asyncGenerators',
+  'classProperties',
+  'classPrivateProperties',
+  'decorators',
+  'doExpressions',
+  'estree',
+  'exportExtensions',
+  'flow',
+  'functionSent',
+  'functionBind',
+  'jsx',
+  'objectRestSpread',
+  'dynamicImport',
+];
+
+const ID = 'babylon7';
+export const defaultOptions = {
+  sourceType: 'module',
+  allowImportExportEverywhere: false,
+  allowReturnOutsideFunction: false,
+  plugins: [
+    'asyncGenerators',
+    'classProperties',
+    'decorators',
+    'doExpressions',
+    'exportExtensions',
+    'flow',
+    'functionSent',
+    'functionBind',
+    'jsx',
+    'objectRestSpread',
+    'dynamicImport',
+  ],
+};
+
+export const parserSettingsConfiguration = {
+  fields: [
+    ['sourceType', ['module', 'script']],
+    'allowReturnOutsideFunction',
+    'allowImportExportEverywhere',
+    {
+      key: 'plugins',
+      title: 'Plugins',
+      fields: availablePlugins,
+      settings: settings => settings.plugins || defaultOptions.plugins,
+      values: plugins => availablePlugins.reduce(
+        (obj, name) => ((obj[name] = plugins.indexOf(name) > -1), obj),
+        {}
+      ),
+    },
+  ],
+};
+
+export default {
+  ...defaultParserInterface,
+
+  id: ID,
+  displayName: ID,
+  version: pkg.version,
+  homepage: pkg.homepage,
+  locationProps: new Set(['loc', 'start', 'end']),
+
+  loadParser(callback) {
+    require(['babylon7'], callback);
+  },
+
+  parse(babylon, code, options) {
+    return babylon.parse(code, {...defaultOptions, ...options});
+  },
+
+  getNodeName(node) {
+    switch (typeof node.type) {
+      case 'string':
+        return node.type;
+      case 'object':
+        return `Token (${node.type.label})`;
+    }
+  },
+
+  nodeToRange(node) {
+    if (typeof node.start !== 'undefined') {
+      return [node.start, node.end];
+    }
+  },
+
+  renderSettings(parserSettings, onChange) {
+    return (
+      <SettingsRenderer
+        settingsConfiguration={parserSettingsConfiguration}
+        parserSettings={{...defaultOptions, ...parserSettings}}
+        onChange={onChange}
+      />
+    );
+  },
+};
