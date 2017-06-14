f3331a6654f401ed68ce554ec691748ed0c78810
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2017-06-14 15:18:38 +0800

    add GLSL language

---
+++ website/src/parsers/glsl/index.js
@@ -0,0 +1,6 @@
+import './codemirror-mode/glsl';
+
+export const id = 'glsl';
+export const displayName = 'GLSL';
+export const mimeTypes = ['x-shader/x-vertex', 'x-shader/x-fragment'];
+export const fileExtension = 'glsl';
