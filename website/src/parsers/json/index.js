03a3912ec7a18633b5edbeb878eac5b8aa76e9d7
Vlad Trushin <monospectr@gmail.com>
2017-11-15 17:51:18 +0300

    Updated json-to-ast parser to 2.0.2 (#281)

--- website/src/parsers/json/index.js
+++ website/src/parsers/json/index.js
@@ -2,5 +2,5 @@ import 'codemirror/mode/javascript/javascript';
 
 export const id = 'json';
 export const displayName = 'JSON';
-export const mimeTypes = ['text/javascript'];
+export const mimeTypes = ['application/json'];
 export const fileExtension = 'json';
