9f16ff51543773f5c3b5cde7a283b83531dd36e6
cowchimp <cowchimp@users.noreply.github.com>
2017-11-27 19:55:09 +0200

    try to make PRODUCTION build pass

--- website/webpack.config.js
+++ website/webpack.config.js
@@ -161,6 +161,7 @@ module.exports = Object.assign({
           path.join(__dirname, 'node_modules', 'typescript-eslint-parser'),
           path.join(__dirname, 'node_modules', 'webidl2'),
           path.join(__dirname, 'node_modules', 'tslint'),
+          path.join(__dirname, 'node_modules', 'tslib'),
           path.join(__dirname, 'src'),
         ],
         loader: 'babel-loader',
