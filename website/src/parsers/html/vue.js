2c4f04007d489769df9046e19646a66f5c711c09
Jamaks <jamakrus@gmail.com>
2019-03-17 20:34:25 +0300

    fix node range

---
+++ website/src/parsers/html/vue.js
@@ -0,0 +1,49 @@
+import defaultParserInterface from '../utils/defaultParserInterface';
+import pkg from 'vue-template-compiler/package.json';
+
+const ID = 'vue';
+
+export default {
+  ...defaultParserInterface,
+
+  id: ID,
+  displayName: ID,
+  version: pkg.version,
+  homepage: pkg.homepage,
+  locationProps: new Set(['start', 'end']),
+
+  loadParser(callback) {
+    require(['vue-template-compiler/browser'], callback);
+
+  },
+
+  parse(parser, code, options) {
+    return parser.compile(code, options).ast;
+  },
+
+  nodeToRange(node) {
+    if (node.type || node.name) {
+      return [node.start, node.end];
+    }
+  },
+
+  opensByDefault(node, key) {
+    return key === 'children';
+  },
+
+  getNodeName(node) {
+    let nodeName = node.tag;
+    if (nodeName && node.name) {
+      nodeName += `(${node.name})`;
+    }
+    return nodeName;
+  },
+
+  getDefaultOptions() {
+    return {
+      outputSourceRange: true,
+      whitespace: 'preserve'
+    };
+  },
+  _ignoredProperties: new Set(['parent']),
+};
