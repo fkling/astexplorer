488a092fbd33ce60c1863c3de8878732767f55b2
Felix Kling <felix.kling@gmx.net>
2019-04-24 01:15:00 -0700

    Remember selected tree view settings in local storage
    A couple of people have asked for persisting these settings across page
    loads.

--- website/src/components/visualization/Tree.js
+++ website/src/components/visualization/Tree.js
@@ -6,83 +6,89 @@ import {logEvent} from '../../utils/logger';
 
 import './css/tree.css'
 
-export default class Tree extends React.Component {
-  constructor(props) {
-    super(props);
+const {useReducer} = React;
 
-    this.state = {autofocus: true, hideFunctions: true};
-  }
+const STORAGE_KEY = 'tree_settings';
 
-  _setOption(name, event) {
-    this.setState({[name]: event.target.checked});
-    logEvent(
-      'tree_view_settings',
-      event.target.checked ? 'enabled' : 'disabled',
-      name
-    );
-  }
+function initSettings() {
+  const storedSettings = global.localStorage.getItem(STORAGE_KEY);
+  return storedSettings ?
+    JSON.parse(storedSettings) :
+    {
+      autofocus: true,
+      hideFunctions: true,
+      hideEmptyKeys: false,
+      hideLocationData: false,
+      hideTypeKeys: false,
+    };
+}
 
-  render() {
-    return (
-      <div className="tree-visualization container">
-        <div className="toolbar">
-          <label title="Auto open the node at the cursor in the source code">
-            <input
-              type="checkbox"
-              checked={this.state.autofocus}
-              onChange={this._setOption.bind(this, 'autofocus')}
-            />
-            Autofocus
-          </label>
-          &#8203;
-          <label>
-            <input
-              type="checkbox"
-              checked={this.state.hideFunctions}
-              onChange={this._setOption.bind(this, 'hideFunctions')}
-            />
-            Hide methods
-          </label>
-          &#8203;
-          <label>
-            <input
-              type="checkbox"
-              checked={this.state.hideEmptyKeys}
-              onChange={this._setOption.bind(this, 'hideEmptyKeys')}
-            />
-            Hide empty keys
-          </label>
-          &#8203;
-          <label>
-            <input
-              type="checkbox"
-              checked={this.state.hideLocationData}
-              onChange={this._setOption.bind(this, 'hideLocationData')}
-            />
-            Hide location data
-          </label>
-          &#8203;
-          <label>
-            <input
-              type="checkbox"
-              checked={this.state.hideTypeKeys}
-              onChange={this._setOption.bind(this, 'hideTypeKeys')}
-            />
-            Hide type keys
-          </label>
-        </div>
-        <ul onMouseLeave={() => {PubSub.publish('CLEAR_HIGHLIGHT');}}>
-          <Element
-            focusPath={this.props.focusPath}
-            value={this.props.ast}
-            level={0}
-            parser={this.props.parser}
-            settings={this.state}
-          />
-        </ul>
+function reducer(state, element) {
+  const newState = {...state, [element.name]: element.checked};
+
+  global.localStorage.setItem(STORAGE_KEY, JSON.stringify(newState));
+  logEvent(
+    'tree_view_settings',
+    element.checked ? 'enabled' : 'disabled',
+    element.name
+  );
+
+  return newState;
+}
+
+function makeCheckbox(name, settings, updateSettings) {
+  return (
+    <input
+      type="checkbox"
+      name={name}
+      checked={settings[name]}
+      onChange={event => updateSettings(event.target)}
+    />
+  );
+}
+
+export default function Tree({focusPath, ast, parser}) {
+  const [settings, updateSettings] = useReducer(reducer, null, initSettings);
+
+  return (
+    <div className="tree-visualization container">
+      <div className="toolbar">
+        <label title="Auto open the node at the cursor in the source code">
+          {makeCheckbox('autofocus', settings, updateSettings)}
+          Autofocus
+        </label>
+        &#8203;
+        <label>
+          {makeCheckbox('hideFunctions', settings, updateSettings)}
+          Hide methods
+        </label>
+        &#8203;
+        <label>
+          {makeCheckbox('hideEmptyKeys', settings, updateSettings)}
+          Hide empty keys
+        </label>
+        &#8203;
+        <label>
+          {makeCheckbox('hideLocationData', settings, updateSettings)}
+          Hide location data
+        </label>
+        &#8203;
+        <label>
+          {makeCheckbox('hideTypeKeys', settings, updateSettings)}
+          Hide type keys
+        </label>
       </div>
-    );
-  }
+      <ul onMouseLeave={() => {PubSub.publish('CLEAR_HIGHLIGHT');}}>
+        <Element
+          focusPath={focusPath}
+          value={ast}
+          level={0}
+          parser={parser}
+          settings={settings}
+        />
+      </ul>
+    </div>
+  );
 }
 
 Tree.propTypes = {
