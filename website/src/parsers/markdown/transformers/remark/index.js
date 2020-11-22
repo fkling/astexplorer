0e12f3c8fa3188eb4636f037801d21b9f80de4e4
Christian Murphy <christian.murphy.42@gmail.com>
2020-11-22 10:25:49 -0700

    use require to load utilities, add comments to code example for remark transform

--- website/src/parsers/markdown/transformers/remark/index.js
+++ website/src/parsers/markdown/transformers/remark/index.js
@@ -1,20 +1,5 @@
 import compileModule from '../../../utils/compileModule';
 import pkg from 'remark/package.json';
-import unistUtilIs from 'unist-util-is';
-import unistUtilVisit from 'unist-util-visit';
-import unistUtilVisitParents from 'unist-util-visit-parents';
-
-const availableModules = {
-  'unist-util-is': unistUtilIs,
-  'unist-util-visit': unistUtilVisit,
-  'unist-util-visit-parents': unistUtilVisitParents,
-};
-
-function sandboxRequire(name) {
-  if (!Object.getOwnPropertyNames(availableModules).includes(name))
-    throw new Error(`Cannot find module '${name}'`);
-  return availableModules[name];
-}
 
 const ID = 'remark';
 
@@ -27,12 +12,33 @@ export default {
   defaultParserID: ID,
 
   loadTransformer(callback) {
-    require(['remark'], (remark) => {
-      callback({ remark });
+    require([
+      'remark',
+      'unist-util-is',
+      'unist-util-visit',
+      'unist-util-visit-parents',
+    ], (remark, unistUtilIs, unistUtilVisit, unistUtilVisitParents) => {
+      callback({ remark, unistUtilIs, unistUtilVisit, unistUtilVisitParents });
     });
   },
 
-  transform({ remark }, transformCode, code) {
+  transform(
+    { remark, unistUtilIs, unistUtilVisit, unistUtilVisitParents },
+    transformCode,
+    code,
+  ) {
+    const availableModules = {
+      'unist-util-is': unistUtilIs,
+      'unist-util-visit': unistUtilVisit,
+      'unist-util-visit-parents': unistUtilVisitParents,
+    };
+
+    function sandboxRequire(name) {
+      if (!Object.getOwnPropertyNames(availableModules).includes(name))
+        throw new Error(`Cannot find module '${name}'`);
+      return availableModules[name];
+    }
+
     const transform = compileModule(transformCode, { require: sandboxRequire });
     return remark().use(transform).processSync(code).contents;
   },
