612a5542e42ccb78b56bf2835da53c9e73911811
justjavac <justjavac@gmail.com>
2021-11-21 00:41:25 +0800

    remove unused pkg

--- website/src/parsers/transpilers/babel.js
+++ website/src/parsers/transpilers/babel.js
@@ -1,12 +1,11 @@
 import * as babel from 'babel-core';
 import es2015 from 'babel-preset-es2015';
 import stage0 from 'babel-preset-stage-0';
-import flowStripTypes from 'babel-plugin-transform-flow-strip-types';
 import protect from '../utils/protectFromLoops';
 
 const options = {
   presets: [es2015, stage0],
-  plugins: [flowStripTypes],
+  plugins: [],
   ast: false,
   babelrc: false,
   highlightCode: false,
