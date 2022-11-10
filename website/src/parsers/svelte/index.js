277b734df2bd905650754e4ddc29f0f86dbabaf5
metonym <ericyl.us@gmail.com>
2022-11-10 10:53:02 -0800

    Add Svelte as a top-level category (#664)

---
+++ website/src/parsers/svelte/index.js
@@ -0,0 +1,6 @@
+import 'codemirror/mode/htmlmixed/htmlmixed';
+
+export const id = 'svelte';
+export const displayName = 'Svelte';
+export const mimeTypes = ['text/html'];
+export const fileExtension = 'svelte';
